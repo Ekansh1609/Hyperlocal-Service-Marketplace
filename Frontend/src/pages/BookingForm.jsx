14f63584db50a6d0366895c341560c4f89198a0c
RohitSharma97714 <rohitkumar.pr45@gmail.com>
2025-05-23 21:42:49 +0530

    Update: Updated

--- Frontend/src/pages/BookingForm.jsx
+++ Frontend/src/pages/BookingForm.jsx
@@ -1,7 +1,6 @@
 // src/pages/BookingForm.jsx
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-import professionData from "../data/ProfessionData";
 
 function BookingForm() {
   const { proId } = useParams();
